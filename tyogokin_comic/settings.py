0c8a1fac7fac93c1703fa8188f20ece9d9fe151a
tyakyumyou-soviet <taizu61zx@gmail.com>
2025-03-14 12:48:09 +0900

    change

--- tyogokin_comic/settings.py
+++ tyogokin_comic/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=3(y@9ozc2atzf_hnh&85da7r2vac)wpe*&c3443-to=%wy@_1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
